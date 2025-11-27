afe5434f30459c67b52a5acb7fffbc28df2aec0a
Liuwei Luo <qian_27@hotmail.com>
2025-11-27 11:42:34 +0200

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="py-8 text-center text-gray-400 border-t border-gray-800">
-      <p>© 2025 Liuwei Luo</p>
-    </footer>
-  );
-}
